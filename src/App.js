962741aed50743200034a94e9023c88f19dfc1f9
Prasanna Subramaniam <prasannatiruppur@gmail.com>
2020-12-17 21:29:24 +0530

    Added snake and ladder

--- src/App.js
+++ src/App.js
@@ -11,32 +11,74 @@ class App extends React.Component {
         X: 0,
         Y: 0,
       },
-      diceNumber: 0,
+      xDiceNumber: 0,
+      yDiceNumber: 0,
       result: null,
     };
     this.state = JSON.parse(JSON.stringify(this.initialState));
   }
+  checkLS = (score) => {
+    switch (score) {
+      case 1:
+        return 23;
+      case 5:
+        return 14;
+      case 21:
+        return 42;
+      case 29:
+        return 85;
+      case 36:
+        return 58;
+      case 68:
+        return 89;
+      case 78:
+        return 99;
+      case 25:
+        return 3;
+      case 35:
+        return 10;
+      case 47:
+        return 7;
+      case 57:
+        return 38;
+      case 77:
+        return 52;
+      case 97:
+        return 61;
+      default:
+        return score;
+    }
+  };
   rolldice = () => {
-    let { currentPlayer } = this.state;
-    let { score } = this.state;
+    let { currentPlayer, score, xDiceNumber, yDiceNumber } = this.state;
     let diceNumber = Math.ceil(Math.random() * 6);
     score[currentPlayer] += diceNumber;
+    if (currentPlayer === "X") {
+      xDiceNumber = diceNumber;
+    }
+    if (currentPlayer === "Y") {
+      yDiceNumber = diceNumber;
+    }
     if (score[currentPlayer] > this.totalCount) {
       score[currentPlayer] -= diceNumber;
       this.setState({
-        diceNumber,
+        xDiceNumber,
+        yDiceNumber,
         currentPlayer: currentPlayer === "X" ? "Y" : "X",
         score,
       });
     } else if (score[currentPlayer] === this.totalCount) {
       this.setState({
-        diceNumber,
+        xDiceNumber,
+        yDiceNumber,
         score,
         result: `Player ${currentPlayer} WON`,
       });
     } else {
+      score[currentPlayer] = this.checkLS(score[currentPlayer]);
       this.setState({
-        diceNumber,
+        xDiceNumber,
+        yDiceNumber,
         currentPlayer: currentPlayer === "X" ? "Y" : "X",
         score,
       });
@@ -50,8 +92,8 @@ class App extends React.Component {
     return (
       <div className="square">
         <span>{squareIndex}</span>
-        {squareIndex === score["X"] ? "X" : ""}
-        {squareIndex === score["Y"] ? "Y" : ""}
+        {squareIndex === score["X"] ? <div className="player-x">X</div> : ""}
+        {squareIndex === score["Y"] ? <div className="player-y">Y</div> : ""}
       </div>
     );
   };
@@ -83,10 +125,36 @@ class App extends React.Component {
     }
     return <div className="board">{renderedRows}</div>;
   };
+  renderLadders = () => {
+    return (
+      <div className="ladders">
+        <div class="ladder-1 ladder"></div>
+        <div class="ladder-2 ladder"></div>
+        <div class="ladder-3 ladder"></div>
+        <div class="ladder-4 ladder"></div>
+        <div class="ladder-5 ladder"></div>
+        <div class="ladder-6 ladder"></div>
+        <div class="ladder-7 ladder"></div>
+        <div class="ladder-8 ladder"></div>
+      </div>
+    );
+  };
+  renderSnakes = () => {
+    return (
+      <div className="snakes">
+        <div class="snake-1 snake"></div>
+        <div class="snake-2 snake"></div>
+        <div class="snake-3 snake"></div>
+        <div class="snake-4 snake"></div>
+        <div class="snake-5 snake"></div>
+        <div class="snake-6 snake"></div>
+      </div>
+    );
+  };
   render() {
     return (
-      <div className="app-container">
-        <div className="result-container unselectable">
+      <div className="app-container unselectable">
+        <div className="result-container">
           {this.state.result ? (
             this.state.result
           ) : (
@@ -94,15 +162,18 @@ class App extends React.Component {
               <div className="result">
                 Player {this.state.currentPlayer}'s Turn
               </div>
+              <div className="dice-number">X : {this.state.xDiceNumber}</div>
               <div className="roll-button button" onClick={this.rolldice}>
                 Roll Dice
               </div>
-              <div className="dice-number">{this.state.diceNumber}</div>
+              <div className="dice-number">Y : {this.state.yDiceNumber}</div>
             </>
           )}
         </div>
         <div className="board-container">
           {this.renderBoard(this.props.rowsCount)}
+          {this.renderLadders()}
+          {this.renderSnakes()}
         </div>
         {this.state.result ? (
           <div className="reset-button button" onClick={this.resetBoard}>

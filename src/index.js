dcb3f35ee3705b94909f9bb032e74692118c80c7
Prasanna Subramaniam <prasannatiruppur@gmail.com>
2020-12-17 13:00:47 +0530

    Spiral Board Completed

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App rowsCount={5} />
+    <App rowsCount={10} />
   </React.StrictMode>,
   document.getElementById("root")
 );
